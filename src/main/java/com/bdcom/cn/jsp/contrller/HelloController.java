f05cd2396bb5f6697ff4e14a696ffe23fae3db82
cjzm888999 <404436080@qq.com>
2018-07-03 13:29:06 +0800

    Initial commit

---
+++ src/main/java/com/bdcom/cn/jsp/contrller/HelloController.java
@@ -0,0 +1,28 @@
+package com.bdcom.cn.jsp.contrller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+/**
+ * on 2016/9/9.
+ */
+@Controller
+
+public class HelloController {
+
+    // 从 application.properties 中读取配置，如取不到默认值为Hello
+    @Value("${application.hello:Hello}")
+    private String hello;
+
+
+    @RequestMapping("/helloJsp")
+    public String helloJsp(Map<String, Object> map) {
+        System.out.println("HelloController.helloJsp().hello=" + hello);
+        map.put("hello", hello);
+        return "helloJsp";
+
+    }
+
+}
